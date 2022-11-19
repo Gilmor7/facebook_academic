d649e490c5194758cbf09c5c97021c81e4200c22
Eli Freid <eli.freid@gmail.com>
2022-11-19 14:00:11 +0200

    changed the eOptions array to a single string message

--- Menu.cpp
+++ Menu.cpp
@@ -1,32 +1,30 @@
 
 #include "Menu.h"
 
-const char* eOptionMessages[] =
-{
-        "1. Add new user to the system\n",
-        "2. Add new fanPage to the system.\n",
-        "3. Add new status to user/fanPage.\n",
-        "4. Show all statuses of user/fanPage. \n",
-        "5. Show 10 most recent statuses from specific user's friends. \n",
-        "6. Connect 2 users.\n",
-        "7. Remove friendship between 2 users\n",
-        "8. Add a user to a fanPage.\n",
-        "9. Remove a user from a fanPage.\n",
-        "10. Show All the registered entities in the system.\n",
-        "11. Show all the friends/followers of a user/fanPage.\n",
-        "12. Exit the Program.\n"
-};
+const char* DISPLAY_MENU_MESSAGE=
+        "1. Add new user to the system\n"
+        "2. Add new fanPage to the system.\n"
+        "3. Add new status to user/fanPage.\n"
+        "4. Show all statuses of user/fanPage.\n"
+        "5. Show 10 most recent statuses from specific user's friends.\n"
+        "6. Connect 2 users.\n"
+        "7. Remove friendship between 2 users\n"
+        "8. Add a user to a fanPage.\n"
+        "9. Remove a user from a fanPage.\n"
+        "10. Show All the registered entities in the system.\n"
+        "11. Show all the friends/followers of a user/fanPage.\n"
+        "12. Exit the Program.\n";
+
 
 void Menu::displayMenu()
 {
-    int i;
-    for(i=0; i<12; i++)
-    {
-        cout << eOptionMessages[i];
-    }
+    cout << DISPLAY_MENU_MESSAGE;
 }
 
 void Menu::setChoiceFromUserInput()
 {
-
+    int option;
+    cout << "Select an option from the above: ";
+    cin >> option;
+    currOption = (Menu::eOption) option;
 }
--- main.cpp
+++ main.cpp
@@ -6,6 +6,7 @@ int main()
 {
     Menu m;
     m.displayMenu();
-
+    m.setChoiceFromUserInput();
+    cout << (int)m.getChoice();
     return 0;
 }
