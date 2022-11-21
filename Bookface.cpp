71c664ae262ac12462ff55053ed96f9b9ebeb78d
Eli Freid <eli.freid@gmail.com>
2022-11-21 23:09:46 +0200

    created some all of the needed (I think) methods of Bookface.h , and did some progress in Bookface.cpp

--- Bookface.cpp
+++ Bookface.cpp
@@ -9,6 +9,28 @@ BookFace::BookFace(): menu()
     this->is_running = true;
 }
 
+bool BookFace::addUser()
+{
+    char name[MAX_NAME_LEN];
+    int day, month, year;
+    cout << "Enter name: [max 50 characters]";
+    cin >> name;
+    cout << "Enter birth date (day month year): ";
+    cin >> day >> month >> year;
+    if(this->isEntityInArr(name, true))
+    {
+        cout << "User already exists!" << endl;
+        return false;
+    }
+    if(this->usersLogSize == this->usersPhySize)
+    {
+        reallocArr(true);
+    }
+    this->usersArr[usersLogSize] = new FriendPage(name, Date(day, month, year));
+    this->usersLogSize++;
+    return true;
+}
+
 
 void BookFace::showMenu()
 {
@@ -18,4 +40,63 @@ void BookFace::showMenu()
 void BookFace::setOptionInMenu()
 {
     this->menu.setChoiceFromUserInput();
+}
+
+void BookFace::activateMenuOption()
+{
+    switch(this->menu.getChoice())
+    {
+        case Menu::eOption::ADD_USER:
+            addUser();
+            break;
+        case Menu::eOption::ADD_PAGE:
+            break;
+        case Menu::eOption::ADD_STATUS:
+            break;
+        case Menu::eOption::SHOW_ENTITY_STATUSES:
+            break;
+        case Menu::eOption::SHOW_LAST_STATUSES:
+            break;
+        case Menu::eOption::CONNECT_USERS:
+            break;
+        case Menu::eOption::REMOVE_USERS_CONNECTION:
+            break;
+        case Menu::eOption::ADD_USER_TO_PAGE:
+            break;
+        case Menu::eOption::REMOVE_USER_FROM_PAGE:
+            break;
+        case Menu::eOption::SHOW_ALL_ENTITIES:
+            break;
+        case Menu::eOption::SHOW_ALL_FOLLOWERS_OF_ENTITY:
+            break;
+        case Menu::eOption::EXIT:
+            break;
+    }
+
+}
+
+void BookFace::reallocArr(bool isUserArr)
+{
+    if(isUserArr)
+    {
+        FriendPage** temp = new FriendPage*[this->usersPhySize * 2];
+        for(int i = 0; i < this->usersLogSize; i++)
+        {
+            temp[i] = this->usersArr[i];
+        }
+        delete[] this->usersArr;
+        this->usersArr = temp;
+        this->usersPhySize *= 2;
+    }
+    else
+    {
+        FanPage** temp = new FanPage*[this->fanPagePhySize * 2];
+        for(int i = 0; i < this->fanPageLogSize; i++)
+        {
+            temp[i] = this->fanPagesArr[i];
+        }
+        delete[] this->fanPagesArr;
+        this->fanPagesArr = temp;
+        this->fanPagePhySize *= 2;
+    }
 }
--- Bookface.h
+++ Bookface.h
@@ -3,15 +3,29 @@
 #define FACEBOOK_ACADEMIC_BOOKFACE_H
 
 #include "Menu.h"
+#include "friend_page.h"
+#include "page.h"
+
+const int MAX_NAME_LEN = 50;
 
 class BookFace
 {
 private:
-    // users Arr
-    // fanPages arr
-    // numOf... (logical and physical)
+    FriendPage** usersArr;
+    FanPage** fanPagesArr;
+    int usersLogSize, usersPhySize, fanPageLogSize, fanPagePhySize;
     Menu menu;
     bool is_running;
+
+    // menu functions (can be called only from activateMenuOption()
+    bool addUser();
+    bool addPage();
+    void showAllRegistered() const;
+    bool connectUsers(const char* name1, const char* name2);
+    bool removeUsersConnection(const char* name1, const char* name2);
+    bool followFanPage(const char* userName, const char* fanPage);
+    bool unfollowFanPage(const char* userName, const char* fanPage);
+    bool showAllFromPage(const char* pageName);
 public:
     BookFace();   // initialize some users and pages here  (in the future, load from files)
 
@@ -26,8 +40,12 @@ public:
     void showMenu();
     void setOptionInMenu();
 
+    // this function will user the menu.choice and activate the private method
     void activateMenuOption();
+
+    bool isEntityInArr(const char* nameOfEntity, bool isUser);
     bool getRunningState() const {return this->is_running;}
+    void reallocArr(bool isUserArr);
 };
 
 #endif //FACEBOOK_ACADEMIC_BOOKFACE_H
