46338b28ad76d9ff12bf65c16f28f0f2c3cb57c1
Eli Freid <eli.freid@gmail.com>
2022-11-21 15:39:25 +0200

    created FanPage class derived from Page

---
+++ FanPage.cpp
@@ -0,0 +1,67 @@
+
+#include "FanPage.h"
+#include <iostream>
+using namespace std;
+
+FanPage::FanPage(const char* pageName) : Page(pageName)
+{
+    cout << "FanPage constructor" << endl;
+    this->followers = new FriendPage*[1];
+    this->followersLogSize = 1;
+    this->followersPhySize = 0;
+}
+
+FanPage::FanPage(const FanPage &other) : Page(other)
+{
+    cout << "FanPage copy constructor" << endl;
+    this->followers = new FriendPage*[other.followersPhySize];
+    this->followersLogSize = other.followersLogSize;
+    this->followersPhySize = other.followersPhySize;
+
+    for (int i = 0; i < other.followersLogSize; i++)
+    {
+        this->followers[i] = other.followers[i];
+    }
+}
+
+FanPage::~FanPage()
+{
+    cout << "FanPage destructor" << endl;
+    delete[] this->followers;
+}
+
+void FanPage::addFollower(FriendPage &user)
+{
+    cout << "FanPage::addFollower" << endl;
+    if (this->followersLogSize == this->followersPhySize)
+    {
+        this->doubleFollowersSize();
+    }
+
+    this->followers[this->followersLogSize] = &user;
+    this->followersLogSize++;
+}
+
+void FanPage::doubleFollowersSize()
+{
+    cout << "FanPage::doubleFollowersSize" << endl;
+    int i;
+    this->followersPhySize *= 2;
+    FriendPage** newArr = new FriendPage* [this->followersPhySize];
+    for(i=0; i<this->followersLogSize; i++)
+    {
+        newArr[i] = this->followers[i];
+    }
+    delete[] this->followers;
+    this->followers = newArr;
+}
+
+void FanPage::showFollowers() const
+{
+    cout << "FanPage::showFollowers" << endl;
+    int i;
+    for(i=0; i<this->followersLogSize; i++)
+    {
+        cout << this->followers[i]->getName() << endl;
+    }
+}
---
+++ FanPage.h
@@ -0,0 +1,24 @@
+
+#ifndef FACEBOOK_ACADEMIC_FANPAGE_H
+#define FACEBOOK_ACADEMIC_FANPAGE_H
+
+#include "friend_page.h"
+
+class FanPage: public Page
+{
+private:
+    FriendPage** followers;
+    int followersLogSize, followersPhySize;
+public:
+
+    FanPage(const char* pageName);
+    FanPage(const FanPage& other);
+    ~FanPage();
+
+    void addFollower(FriendPage& user);
+    void showFollowers() const;
+    void doubleFollowersSize();
+};
+
+
+#endif //FACEBOOK_ACADEMIC_FANPAGE_H
