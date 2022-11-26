e2c6537a863b873fb111783b91a60aba48319719
Eli Freid <eli.freid@gmail.com>
2022-11-26 20:43:08 +0200

    wrote some basic tests in main

--- main.cpp
+++ main.cpp
@@ -4,5 +4,30 @@ using namespace std;
 
 int main()
 {
+    FriendPage* f1 = new FriendPage("Eli", Date(12,9,1998));
+    FriendPage* f2 = new FriendPage("Bob", Date(6,6,1666));
 
+    FanPage* p1 = new FanPage("John cena fans");
+    FanPage* p2 = new FanPage("golden state warriors");
+
+    {
+        BookFace system;
+        system.addUser(*f1);
+        system.addUser(*f2);
+
+        system.addUser(*f1);   // should give error msg and not add
+
+        system.addPage(*p1);
+        system.addPage(*p2);
+
+        system.addPage(*p2);  // should give error msg
+
+        system.connectUsers(*f1, *f2);
+        system.connectUsers(*f1, *f2);  // should give msg users are connected
+
+        system.removeUsersConnection(*f1, *f2);
+        system.removeUsersConnection(*f1, *f2); // should give msg users not connected
+
+
+    }
 }
