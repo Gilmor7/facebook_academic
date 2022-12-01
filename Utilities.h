e9837d54e2820b72f4681d2358f53365d952f0e3
Eli Freid <eli.freid@gmail.com>
2022-12-01 21:22:07 +0200

    removed testing functions

--- Utilities.h
+++ Utilities.h
@@ -8,8 +8,5 @@ FriendPage* getFriendByNameFromSystem(const char* name, BookFace& system);
 FanPage* getFanPageByNameFromSystem(const char* name, BookFace& system);
 void initilizeStartingDataIntoSystem(BookFace& system);
 
-// testing functions
-void gilTest();
-void EliTest();
 
 #endif //FACEBOOK_ACADEMIC_UTILITIES_H
