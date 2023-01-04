01e358cdec4782811208fc63e1444bd3bfed78f1
Eli Freid <eli.freid@gmail.com>
2023-01-04 19:26:44 +0200

    created Page abstract class

---
+++ Page.cpp
@@ -0,0 +1,60 @@
+
+#include "Page.h"
+
+Page::Page(const string &pageName) noexcept(false)
+{
+//    if(pageName == "")
+//        throw CreatePageException
+    this->pageName = pageName;
+}
+
+Page::~Page()
+{
+    auto itr = this->statuses.begin();
+    auto itrEnd = this->statuses.end();
+    for(; itr != itrEnd; ++itr)
+    {
+        delete *itr;
+    }
+}
+
+void Page::showFollowers() const
+{
+    auto itr = this->followers.begin();
+    auto itrEnd = this->followers.end();
+    for(; itr != itrEnd; ++itr)
+    {
+        (*itr)->showName();
+    }
+}
+
+void Page::showStatuses(int amount) const
+{
+    auto itr = this->statuses.begin();
+    auto itrEnd = this->statuses.end();
+    for(; itr != itrEnd; ++itr)
+    {
+        (*itr)->showStatus();
+    }
+}
+
+void Page::addStatus(const Status &newStatus)
+{
+    this->statuses.push_back(new Status(newStatus));    // need to check if working properly
+}
+
+
+
+void Page::showName() const
+{
+    cout << this->pageName << endl;
+}
+
+bool Page::operator>(const Page &other) const
+{
+    int p1Size = this->followers.size();
+    int p2Size = other.followers.size();
+    if(p1Size > p2Size)
+        return true;
+    return false;
+}
---
+++ Page.h
@@ -0,0 +1,39 @@
+
+#ifndef FACEBOOK_ACADEMIC_PAGE_H
+#define FACEBOOK_ACADEMIC_PAGE_H
+#include <string.h>
+#include <iostream>
+#include "Status.h"
+#include <vector>
+#include "Exceptions.h"
+#include <algorithm>
+using namespace std;
+
+constexpr int ALL = -1;
+
+class Page
+{
+protected:
+    string pageName;
+    vector<const Page*> followers;      // will use polymorphism to store followers/friends (depend on the page)
+    vector<Status*> statuses;           // * for later support of polymorphism in statuses
+
+    // make Page abstract class
+    Page(const string& pageName) noexcept(false);
+public:
+    // virtual D'tor
+    virtual ~Page();
+
+    // virtual methods
+    virtual void showName() const;
+
+
+    // regular methods
+    void showStatuses(int amount = ALL) const;
+    void showFollowers() const;
+    void addStatus(const Status& newStatus);
+    bool operator>(const Page& other) const;
+};
+
+
+#endif //FACEBOOK_ACADEMIC_PAGE_H
